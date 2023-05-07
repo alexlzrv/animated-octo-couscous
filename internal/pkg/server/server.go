666ce53628b66cc583d7dd241d862f82d0b8668a
mayr0y <alexlzrw@gmail.com>
2023-05-08 01:35:48 +0300

    wip: refactoring with chi

---
+++ internal/pkg/server/server.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"context"
+	"github.com/go-chi/chi/v5"
+	"github.com/mayr0y/animated-octo-couscous.git/internal/pkg/storage"
+	"net/http"
+)
+
+type MetricsServer struct {
+	MetricsStore storage.Metric
+	context      context.Context
+}
+
+func (s *MetricsServer) StartListener() {
+	mux := chi.NewRouter()
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		return
+	}
+	RegisterHandlers(mux, s.MetricsStore)
+}
--- internal/pkg/storage/metrics.go
+++ internal/pkg/storage/metrics.go
@@ -8,6 +8,15 @@ const (
 type Gauge float64
 type Counter int64
 
+type Metric interface {
+	UpdateGaugeMetric(name string, value Gauge)
+	UpdateCounterMetric(name string, value Counter)
+	GetGaugeMetric(name string) (Gauge, bool)
+	GetCounterMetric(name string) (Counter, bool)
+	GetGaugeMetrics() map[string]Gauge
+	GetCounterMetrics() map[string]Counter
+}
+
 type Metrics struct {
 	GaugeMetrics   map[string]Gauge
 	CounterMetrics map[string]Counter
@@ -18,4 +27,32 @@ func NewMetrics() *Metrics {
 	metric.GaugeMetrics = make(map[string]Gauge)
 	metric.CounterMetrics = make(map[string]Counter)
 	return &metric
+}
+
+func (m *Metrics) UpdateGaugeMetric(metricName string, metricValue Gauge) {
+	m.GaugeMetrics[metricName] = metricValue
+}
+
+func (m *Metrics) UpdateCounterMetric(metricName string, metricValue Counter) {
+	m.CounterMetrics[metricName] += metricValue
+}
+
+func (m *Metrics) GetGaugeMetric(metricName string) (Gauge, bool) {
+	metric, ok := m.GaugeMetrics[metricName]
+
+	return metric, ok
+}
+
+func (m *Metrics) GetCounterMetric(metricName string) (Counter, bool) {
+	metric, ok := m.CounterMetrics[metricName]
+
+	return metric, ok
+}
+
+func (m *Metrics) GetGaugeMetrics() map[string]Gauge {
+	return m.GaugeMetrics
+}
+
+func (m *Metrics) GetCounterMetrics() map[string]Counter {
+	return m.CounterMetrics
 }
