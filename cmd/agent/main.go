75b169cb91b5fcdb9acb3230dc8310fab1ee899b
mayr0y <alexlzrw@gmail.com>
2023-05-09 20:09:08 +0300

    wip: refactoring, add flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,77 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"github.com/mayr0y/animated-octo-couscous.git/internal/pkg/storage"
+	"flag"
+	"github.com/caarlos0/env"
+	"github.com/mayr0y/animated-octo-couscous.git/internal/pkg/agent"
 	"log"
-	"math/rand"
-	"net/http"
 	"os"
 	"os/signal"
-	"runtime"
-	"strings"
 	"syscall"
 	"time"
 )
 
-type gauge float64
+func main() {
+	config := agent.NewAgentConfig()
 
-func getMetrics() map[string]gauge {
-	var metrics runtime.MemStats
-	runtime.ReadMemStats(&metrics)
+	flag.StringVar(&config.ServerAddress, "a", "localhost:8080", "ServerAddress")
+	flag.IntVar(&config.ReportInterval, "r", 10, "ReportInterval")
+	flag.IntVar(&config.PollInterval, "p", 2, "PollInterval")
+	flag.Parse()
 
-	metricsMap := map[string]gauge{
-		"Alloc":         gauge(metrics.Alloc),
-		"BuckHashSys":   gauge(metrics.BuckHashSys),
-		"Frees":         gauge(metrics.Frees),
-		"GCCPUFraction": gauge(metrics.GCCPUFraction),
-		"GCSys":         gauge(metrics.GCSys),
-		"HeapAlloc":     gauge(metrics.HeapAlloc),
-		"HeapIdle":      gauge(metrics.HeapIdle),
-		"HeapInuse":     gauge(metrics.HeapInuse),
-		"HeapObjects":   gauge(metrics.HeapObjects),
-		"HeapReleased":  gauge(metrics.HeapReleased),
-		"HeapSys":       gauge(metrics.HeapSys),
-		"LastGC":        gauge(metrics.LastGC),
-		"Lookups":       gauge(metrics.Lookups),
-		"MCacheInuse":   gauge(metrics.MCacheInuse),
-		"MCacheSys":     gauge(metrics.MCacheSys),
-		"MSpanInuse":    gauge(metrics.MSpanInuse),
-		"MSpanSys":      gauge(metrics.MSpanSys),
-		"Mallocs":       gauge(metrics.Mallocs),
-		"NextGC":        gauge(metrics.NextGC),
-		"NumForcedGC":   gauge(metrics.NumForcedGC),
-		"NumGC":         gauge(metrics.NumGC),
-		"OtherSys":      gauge(metrics.OtherSys),
-		"PauseTotalNs":  gauge(metrics.PauseTotalNs),
-		"StackInuse":    gauge(metrics.StackInuse),
-		"StackSys":      gauge(metrics.StackSys),
-		"Sys":           gauge(metrics.Sys),
-		"TotalAlloc":    gauge(metrics.TotalAlloc),
-		"RandomValue":   gauge(rand.Float64()),
+	if err := env.Parse(config); err != nil {
+		log.Fatalf(err.Error())
 	}
 
-	return metricsMap
-}
-
-func main() {
 	var pollCount int64
 
-	pollInterval := time.NewTicker(2 * time.Second)
-	defer pollInterval.Stop()
+	pollInterval := time.Duration(config.PollInterval) * time.Second
+	reportInterval := time.Duration(config.ReportInterval) * time.Second
 
-	reportInterval := time.NewTicker(10 * time.Second)
-	defer reportInterval.Stop()
+	pollTicker := time.NewTicker(pollInterval)
+	defer pollTicker.Stop()
+	reportTicker := time.NewTicker(reportInterval)
+	defer reportTicker.Stop()
 
 	go func() {
-		metric := getMetrics()
+		metric := agent.GetMetrics()
 		for {
 			select {
-			case <-pollInterval.C:
+			case <-pollTicker.C:
 				pollCount++
-				metric = getMetrics()
-			case <-reportInterval.C:
-				if err := sendMetrics(metric, pollCount); err != nil {
+				metric = agent.GetMetrics()
+			case <-reportTicker.C:
+				if err := agent.SendMetrics(metric, pollCount, config.ServerAddress); err != nil {
 					log.Println(err.Error())
 				}
 				pollCount = 0
@@ -82,47 +52,4 @@ func main() {
 	signalChanel := make(chan os.Signal, 1)
 	signal.Notify(signalChanel, syscall.SIGINT, syscall.SIGTERM)
 	<-signalChanel
-}
-
-func createURL(address string, metricType string, metricName string, metricValue string) string {
-	str := []string{"http:/", address, "update", metricType, metricName, metricValue}
-	return strings.Join(str, "/")
-}
-
-func createPostRequest(url string) error {
-	req, err := http.NewRequest(http.MethodPost, url, nil)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Set("Content-Type", "text/plain")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	err = resp.Body.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func sendMetrics(metric map[string]gauge, count int64) error {
-	for name, value := range metric {
-		urlCounter := createURL("localhost:8080", storage.CounterMetricName, "PollCount", fmt.Sprintf("%d", count))
-		err := createPostRequest(urlCounter)
-		if err != nil {
-			return err
-		}
-
-		urlGauge := createURL("localhost:8080", storage.GaugeMetricName, name, fmt.Sprintf("%f", float64(value)))
-		err = createPostRequest(urlGauge)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -1,21 +1,27 @@
 package server
 
 import (
-	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/mayr0y/animated-octo-couscous.git/internal/pkg/storage"
 	"net/http"
 )
 
-type MetricsServer struct {
-	MetricsStore storage.Metric
-	context      context.Context
+type Config struct {
+	ServerAddress string `env:"ADDRESS"`
 }
 
-func (s *MetricsServer) StartListener() {
+func NewServerConfig() *Config {
+	return &Config{}
+}
+
+type MetricsServer struct {
+	MetricsStore storage.Metric
+}
+
+func (s *MetricsServer) StartListener(c *Config) {
 	mux := chi.NewRouter()
 	RegisterHandlers(mux, s.MetricsStore)
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(c.ServerAddress, mux)
 	if err != nil {
 		return
 	}
