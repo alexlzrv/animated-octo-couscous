8deb3020ab94e16fa818d4ac0dd5e1d9b3808b1d
mayr0y <alexlzrw@gmail.com>
2023-06-25 22:58:18 +0300

    added new metrics; refactoring agent

--- go.mod
+++ go.mod
@@ -7,17 +7,24 @@ require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/httplog v0.3.0
 	github.com/jackc/pgx/v5 v5.3.1
+	github.com/shirou/gopsutil/v3 v3.23.5
 	github.com/sirupsen/logrus v1.9.2
 )
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rs/zerolog v1.27.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
--- internal/pkg/agent/agent.go
+++ internal/pkg/agent/agent.go
@@ -5,47 +5,40 @@ import (
 	"github.com/mayr0y/animated-octo-couscous.git/internal/pkg/agent/config"
 	"github.com/mayr0y/animated-octo-couscous.git/internal/pkg/storage"
 	"github.com/sirupsen/logrus"
-	"os"
 	"os/signal"
+	"sync"
 	"syscall"
-	"time"
 )
 
 func StartClient(ctx context.Context, c *config.AgentConfig) {
 	logrus.Info("Agent is running...")
-	pollInterval := time.Duration(c.PollInterval) * time.Second     //тесты не проходят с duration
-	reportInterval := time.Duration(c.ReportInterval) * time.Second //тесты не проходят с duration
-
-	pollTicker := time.NewTicker(pollInterval)
-	defer pollTicker.Stop()
-	reportTicker := time.NewTicker(reportInterval)
-	defer reportTicker.Stop()
+	ctx, stop := signal.NotifyContext(ctx,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+	defer stop()
+	wg := sync.WaitGroup{}
 
 	metric := storage.NewMetrics()
 
-	agentContext, cancelCtx := context.WithCancel(ctx)
-	defer cancelCtx()
+	wg.Add(1)
 	go func() {
-		for {
-			select {
-			case <-pollTicker.C:
-				if err := UpdateMetrics(agentContext, metric); err != nil {
-					logrus.Errorf("Error update metrics %v", err)
-				}
-			case <-reportTicker.C:
-				if err := SendMetrics(agentContext, metric, c.ServerAddress, c.SignKey); err != nil {
-					logrus.Errorf("Error send metrics %v", err)
-				}
-				if err := SendMetricsBatch(agentContext, metric, c.ServerAddress); err != nil {
-					logrus.Errorf("Error send metrics batch %v", err)
-				}
-				if err := metric.ResetCounterMetric(agentContext, "PollCount"); err != nil {
-					logrus.Errorf("Error reset metrics %v", err)
-				}
-			}
-		}
+		defer wg.Done()
+		RunUpdateMemStatMetrics(ctx, c, metric)
 	}()
-	signalChanel := make(chan os.Signal, 1)
-	signal.Notify(signalChanel, syscall.SIGINT, syscall.SIGTERM)
-	<-signalChanel
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		RunUpdateVirtualMetrics(ctx, c, metric)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		RunSendMetric(ctx, c, metric)
+	}()
+
+	wg.Wait()
 }
